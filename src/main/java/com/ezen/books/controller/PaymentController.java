b4e1ccdec310ad086b8bbdbd0e1847d8fd3da477
ParkJunHee9327 <qpfm111@naver.com>
2024-12-09 18:19:45 +0900

    backbone

--- src/main/java/com/ezen/books/controller/PaymentController.java
+++ src/main/java/com/ezen/books/controller/PaymentController.java
@@ -4,11 +4,9 @@ import com.ezen.books.domain.CartDto;
 import com.ezen.books.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
