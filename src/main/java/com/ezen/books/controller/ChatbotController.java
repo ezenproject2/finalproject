0facd26aff8829fb72fdb5921911fb89ce2fbaff
alswn3632 <als3632@naver.com>
2024-12-26 11:19:13 +0900

    SSE 알림 구현 화면 연결 완료

--- src/main/java/com/ezen/books/controller/ChatbotController.java
+++ src/main/java/com/ezen/books/controller/ChatbotController.java
@@ -73,29 +73,9 @@ public class ChatbotController {
     private Map<String, String> getFixedResponse(String userMessage) {
         Map<String, String> answer = new HashMap<>();
 
-        if (userMessage.matches("(?i).*안녕.*")) {// "안녕하세요"가 포함된 메시지
-            answer.put("answer", "안녕하세요! 무엇을 도와드릴까요? *도움*을 입력해 메뉴를 볼 수 있어요!");
-            answer.put("classify", "a-type");
-            return answer;
-        }
-
-        if (userMessage.matches("(?i).*누구.*")) { // "날씨"가 포함된 메시지
-            answer.put("answer", "저는 이젠문고의 챗봇 알리미입니다! 반가워요 :) ");
-            answer.put("classify", "a-type");
-            return answer;
-        }
-
-        if (userMessage.matches("(?i).*도움.*")) {
-            answer.put("answer", "1. 책 검색 : 책 제목을 통해 상품 페이지로 연결해드립니다. <br> " +
-                    "2. 포인트 : 당신의 포인트를 검색해 찾아옵니다. <br> " +
-                    "3. 가까운 매장 : 가까운 매장을 찾아 알려줍니다. " +
-                    "4. ");
-            answer.put("classify", "a-type");
-            return answer;
-        }
-
         // "내 포인트" 관련 질문 처리
-        if (userMessage.toLowerCase().contains("포인트")) {
+        if (userMessage.toLowerCase().contains("포인트")
+                || userMessage.toLowerCase().contains("내 포인트")) {
             // 포인트 정보 DB에서 조회
             // pointVO 생기면 그때 열것!!!
             // int points = chatbotService.getUserPoint(userId);
@@ -113,18 +93,48 @@ public class ChatbotController {
         }
 
         // "책 검색해줘" 관련 질문 처리
-        if (userMessage.matches("(?i).*책 검색.*")) {
+        if (userMessage.matches("(?i).*책 검색.*")
+                || userMessage.matches("(?i).*책 찾기.*")
+                || userMessage.matches("(?i).*책 찾고.*")
+                || userMessage.matches("(?i).*책 찾는.*")) {
             answer.put("answer", "무슨 책을 찾고 싶어요? *책 제목만 입력해주세요!*");
             answer.put("classify", "b-type"); // 다음 질문은 b-type으로 분류
             return answer;
         }
 
         // "매장 안내" 관련 질문 처리
-        if (userMessage.matches("(?i).*가까운 매장.*")) {
+        if (userMessage.matches("(?i).*가까운 매장.*")
+                || userMessage.matches("(?i).*근처 매장.*")) {
             answer.put("answer", "가까운 매장을 찾아볼게요! 위치 정보를 받습니다!");
             answer.put("classify", "c-type"); // 다음 질문은 b-type으로 분류
             return answer;
         }
+
+        // 자연어, 이젠문고 ver 답변
+        if (userMessage.matches("(?i).*안녕.*")
+                || userMessage.matches("(?i).*하이.*")
+                || userMessage.matches("(?i).*헬로.*")) {// "안녕하세요"가 포함된 메시지
+            answer.put("answer", "안녕하세요! 무엇을 도와드릴까요? *도움*을 입력해 메뉴를 볼 수 있어요!");
+            answer.put("classify", "a-type");
+            return answer;
+        }
+
+        if (userMessage.matches("(?i).*누구.*")) { // "날씨"가 포함된 메시지
+            answer.put("answer", "저는 이젠문고의 챗봇 알리미입니다! 반가워요 :) ");
+            answer.put("classify", "a-type");
+            return answer;
+        }
+
+        if (userMessage.matches("(?i).*도움.*")
+                || userMessage.matches("(?i).*도와.*")) {
+            answer.put("answer", "1. 책 검색 : 책 제목을 통해 상품 페이지로 연결해드립니다. <br> " +
+                    "2. 포인트 : 당신의 포인트를 검색해 찾아옵니다. <br> " +
+                    "3. 가까운 매장 : 가까운 매장을 찾아 알려줍니다. " +
+                    "4. ");
+            answer.put("classify", "a-type");
+            return answer;
+        }
+
 
         return null; // 고정된 답변이 없으면 null 반환
     }
