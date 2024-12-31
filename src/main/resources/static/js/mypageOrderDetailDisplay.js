29f6733c228b165ddc875f97715cdf5800e08792
ParkJunHee9327 <qpfm111@naver.com>
2024-12-31 11:58:45 +0900

    convert eng status to kr /mypage/order_detail

--- src/main/resources/static/js/mypageOrderDetailDisplay.js
+++ src/main/resources/static/js/mypageOrderDetailDisplay.js
@@ -2,8 +2,36 @@ console.log("mypageOrderDetailDisplay.js recognized.");
 
 document.addEventListener('DOMContentLoaded', () => {
     displayPaymentInfo();
+    showStatusInKorean();
 })
 
+function showStatusInKorean() {
+    let orderSize = document.querySelector(`.order-data-container`).dataset.orderSize;
+    orderSize = parseInt(orderSize);
+    console.log("The order size: ", orderSize);
+
+    for(let i=0; i < orderSize; i++) {
+        let status = document.querySelector(`.order-detail-status[data-order-index="${i}"]`);
+        console.log("The raw status: ", status.innerText);
+        let statusVal = "";
+
+        switch (status.innerText) {
+            case "refunded":
+                statusVal = "환불됨";
+                break;
+            case "completed":
+                statusVal = "주문 완료";
+                break;
+            default:
+                statusVal = "Unknown";
+                break;
+        }
+
+        status.innerText = statusVal;
+    }
+}
+
+// 결제 정보에 입력되는 값을 계산하여 넣는 함수
 function displayPaymentInfo() {
     let sumOriginalPrice = 0;
     let sumSalePrice = 0;
